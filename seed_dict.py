2a616ed013a676da95dac3f567f7c84a02194c3f
Hackbright Student <info@hackbrightacademy.com>
2014-04-28 15:28:41 -0700

    need to check valid suggestion

--- seed_dict.py
+++ seed_dict.py
@@ -1,56 +1,80 @@
-from sys import argv
+# from sys import argv
 from os.path import exists
 import re
-
-script, input_file = argv
-isValid = True
 vowel_pattern = re.compile('[aeiou]')
+def create_key(word):
+    def remove_duplicate_letters(word):
+        out = ''
+        for i in range(len(word) - 1):
+            if word[i] != word[i+1]:
+                out += word[i]
+        # append last character
+        out += word[-1]
+        return out    
 
-if not exists(input_file):
-    print "%r does not exist!" % input_file
-    isValid = False
+    def free_vowels(word):
+        key = ''
+        for char in word:
+            # replace all vowels with free character '_'
+            if vowel_pattern.match(char):
+                char = '_'
+            key += char
+        return key
 
-if isValid:
-    in_file = open(input_file)
+    return free_vowels(remove_duplicate_letters(word))
 
-    # Is cursor at the end of the file? 
-    in_file_ended = False
-    dictionary = {}
-    def create_key(word):
-        def remove_duplicate_letters(word):
-            out = ''
-            for i in range(len(word) - 1):
-                if word[i] != word[i+1]:
-                    out += word[i]
-            # append last character
-            out += word[-1]
-            return out    
+def seed_dict():
+    # script, input_file = argv
+    input_file = "toydict.txt"
+    isValid = True
 
-        def free_vowels(word):
-            key = ''
-            for char in word:
-                # replace all vowels with free character '_'
-                if vowel_pattern.match(char):
-                    char = '_'
-                key += char
-            return key
+    if not exists(input_file):
+        print "%r does not exist!" % input_file
+        isValid = False
 
-        return free_vowels(remove_duplicate_letters(word))
-    def add_to_dictionary(key, value):
-        if dictionary.get(key):
-            dictionary[key].append(value)
-        else:
-            dictionary[key] = [value]
+    if isValid:
+        in_file = open(input_file)
+        in_file_ended = False # Is cursor at the end of the file? 
+        dictionary = {}
 
-    while not in_file_ended:
-        word = in_file.readline().strip()
-        if word == '':
-            in_file_ended = True
-            break
+        def add_to_dictionary(key, value):
+            if dictionary.get(key):
+                dictionary[key].append(value)
+            else:
+                dictionary[key] = [value]
 
-        key = create_key(word)
-        add_to_dictionary(key, word)
+        while not in_file_ended:
+            word = in_file.readline().strip()
+            if word == '':
+                in_file_ended = True
+                break
 
-    print dictionary
+            key = create_key(word)
+            add_to_dictionary(key, word)
 
+        return dictionary
 
+def is_valid_spellcheck(raw, potential):
+    if len(raw) < len(potential):
+        return False
+    return True
+    # for i in range(len(potential)):
+    #     if potential[i]
+
+def give_suggestion(user_input):
+    key = create_key(user_input)
+    if dictionary.get(key):
+        potential = dictionary[key][0]
+        if is_valid_spellcheck(user_input, potential):
+            return potential
+    return "NO SUGGESTION"
+
+dictionary = seed_dict()
+print dictionary
+
+while True: 
+    user_input = raw_input("> ").strip().lower() # case insensitive
+    if not user_input:
+        continue
+    suggesion = give_suggestion(user_input)
+    print suggesion
