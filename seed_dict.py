71aa5ddca90b372045057766ceca5b32d5578f98
Hackbright Student <info@hackbrightacademy.com>
2014-04-28 14:33:57 -0700

    toy

---
+++ seed_dict.py
@@ -0,0 +1,35 @@
+from sys import argv
+from os.path import exists
+import re
+
+script, input_file = argv
+isValid = True
+
+vowel_pattern = re.compile('[aeiou]')
+
+if not exists(input_file):
+    print "%r does not exist!" % input_file
+    isValid = False
+
+if isValid:
+    in_file = open(input_file)
+
+    # Is cursor at the end of the file? 
+    in_file_ended = False
+    dictionary = {}
+
+    while not in_file_ended:
+        word = in_file.readline()
+        if word == '':
+            in_file_ended = True
+            break
+
+    # create key
+    key = ''
+    for char in word:
+        if vowel_pattern.match(char):
+            char = '?'
+        key += char
+    print key    
+
+
